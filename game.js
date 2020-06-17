a85d391c977686c18f9b90bebf0245961fa75c29
Xingbei99 <xingbei.cao@vanderbilt.edu>
2020-06-17 08:26:41 -0500

    Second

--- game.js
+++ game.js
@@ -1,15 +1,38 @@
 alert("Welcome to the game!");
-var buttonColorsList = ["red", "blue", "green", "yellow"];
-var pattern = [];
+var gameStarted = false;
+var curLevel = 0;
 
-nextNumInSeq();
+var buttonColorsList = ["red", "blue", "green", "yellow"];
+
+var sampleSeq = [];
+var userClickSeq = [];
+
+$("body").keypress(function(){
+  if (!gameStarted){
+    nextNumInSeq();
+    gameStarted = true;
+  }
+});
+
+$(".btn").click(function(){
+  var userClickColor = $(this).attr("id");
+  animateButtonPress(userClickColor);
+  playSound(userClickColor);
+
+  userClickSeq.push(userClickColor);
+  console.log(userClickSeq);
+})
 
 // Generates the number of the next button to press.
 function nextNumInSeq(){
+  // increments level count and display that level title at top of the game
+  curLevel++;
+  $("#level-title").text("Level " + curLevel);
+
   // Randomly choose the next color and add it to the pattern.
   var nextNum = Math.floor(Math.random() * 4);
   var nextColor = buttonColorsList[nextNum];
-  pattern.push(nextColor);
+  sampleSeq.push(nextColor);
 
   // Flash the button with the color to notify user of the next color
   $("#" + nextColor).fadeOut(100)
@@ -17,6 +40,19 @@ function nextNumInSeq(){
                     .fadeOut(100)
                     .fadeIn(100);
 
-  var nextColorSound = new Audio("sounds/" + nextColor + ".mp3");
-  nextColorSound.play();
+  playSound(nextColor);
+}
+
+function playSound(colorName){
+  var colorSound = new Audio("sounds/" + colorName + ".mp3");
+  colorSound.play();
+}
+
+// When we press a button, the button displays a shadow lasting for 200 ms.
+function animateButtonPress(colorName){
+  $("#" + colorName).addClass("pressed");
+  // remove the shadow with a delay of 200ms.
+  setTimeout(function(){
+    $("#" + colorName).removeClass("pressed");
+  }, 200);
 }
